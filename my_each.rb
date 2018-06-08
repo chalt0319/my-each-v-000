a2ff936c9b8ada0b392273ca40e64b6e227c2c4e
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-08 17:45:22 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+words = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(array)
+  while array.length > 0 
+    yield
+  end
 end
+
+my_each(words) do |i|
+  counter = 0 
+  puts words[counter]
+  counter += 1 
+end 
